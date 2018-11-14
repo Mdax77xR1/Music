6e8db63ca8524ece1b4d8c20f821784e6d8d3c2d
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-14 10:54:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -362,20 +362,20 @@ client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
     
-if (message.content.startsWith(perfix + 'Playing')) {
+if (message.content.startsWith('Rplaying')) {
   client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else 
-  if (message.content.startsWith(perfix + 'Name')) {
+  if (message.content.startsWith('Rname')) {
 client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-  if (message.content.startsWith(adminprefix + 'Avatar')) {
+  if (message.content.startsWith('Ravatar')) {
 client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else     
-if (message.content.startsWith(adminprefix + 'Steam')) {
+if (message.content.startsWith('Rstream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");//حقوقxR1 Server
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)//حقوق xR1 Server
 }
