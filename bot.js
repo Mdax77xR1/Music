e95b580360d9c1c9529fe7ebd8d09ed7c228edca
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-20 10:49:05 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -356,30 +356,6 @@ j = 1;
 });
 
 
-const adminprefix = "R";
-const devs = ['335484868479811584 '];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'Playing')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else 
-  if (message.content.startsWith(adminprefix + 'Name')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'Avatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else     
-if (message.content.startsWith(adminprefix + 'Stream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");//حقوقxR1 Server
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)//حقوق xR1 Server
-}
-});
 
 
 
