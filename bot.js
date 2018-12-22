f9c219d502afe6926578e1ab45509437694f738d
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-12-21 21:33:27 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -315,7 +315,6 @@ client.on('message', message => {
    if (message.content === 'R1help') {
        let helpEmbed = new Discord.RichEmbed()
        .setTitle('**أوامر الميوزك...**')
-       .setDescription('**برفكس البوت ($)**')
        .addField('play', 'لتشغيل اغنية')
        .addField('join', 'دخول رومك الصوتي')
        .addField('disconnect', 'الخروج من رومك الصوتي')
